ae958ffff28a784f815e73bda95bfa88391f8178
Ramazan Iusupov <iusupov.ramazan.i@gmail.com>
2026-06-30 15:27:46 -0700

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        guard numbers.count > 1 else { return [] }
+        var start = 0
+        var end = numbers.count - 1
+        while start < end {
+            if numbers[start] + numbers[end] < target {
+                start += 1
+            } else if numbers[start] + numbers[end] > target {
+                end -= 1
+            } else {
+                return [start + 1, end + 1]
+            }
+        }
+        return [start + 1, end + 1]
+    }
+}

654b52a38424d47a6dd88fe8ff53b16ecaf13117
Iusupov Ramazan <iusupov.ramazan.i@gmail.com>
2026-07-07 21:28:05 -0700

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func trap(_ height: [Int]) -> Int {
+        var maxLeft = Array(repeating: 0, count: height.count)
+        var maxRight = Array(repeating: 0, count: height.count)
+
+        var left = 0
+        var right = height.count - 1
+        var result = 0
+        for i in 1..<height.count {
+            maxLeft[i] = max(maxLeft[i - 1], height[i - 1])
+        }
+        for i in stride(from: height.count - 2, through: 0, by: -1) {
+            maxRight[i] = max(maxRight[i + 1], height[i + 1])
+        }
+        for i in 0..<height.count {
+            result += max(0, min(maxRight[i], maxLeft[i]) - height[i])
+        }
+        return result
+    }
+}

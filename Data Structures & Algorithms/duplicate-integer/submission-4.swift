3e37ef74a38dda81126739ead4335a9dd5e5e8fc
Iusupov Ramazan <iusupov.ramazan.i@gmail.com>
2026-07-17 11:39:33 -0700

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func hasDuplicate(_ nums: [Int]) -> Bool {
+        var set = Set<Int>()
+        for n in nums {
+            if set.contains(n) {
+                return true
+            }
+            set.insert(n)
+        }
+        return false
+    }
+}

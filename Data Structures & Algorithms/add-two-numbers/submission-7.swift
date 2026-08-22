ef70f42fde8569f70c24282191fea623200daa31
Iusupov Ramazan <iusupov.ramazan.i@gmail.com>
2026-08-21 17:14:35 -0700

    Add: add-two-numbers - submission-7

---
+++ Data Structures & Algorithms/add-two-numbers/submission-7.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     var val: Int
+ *     var next: ListNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var dummy = ListNode(0)
+        var curr: ListNode? = dummy
+        var (l1, l2) = (l1, l2)
+        var add = 0
+
+        while l1 != nil || l2 != nil || add != 0 {
+            let val1 = l1?.val ?? .zero
+            let val2 = l2?.val ?? .zero
+            let sum = val1 + val2 + add
+            add = sum / 10
+            let node = ListNode(sum % 10)
+            curr?.next = node
+            curr = curr?.next
+            l1 = l1?.next
+            l2 = l2?.next
+        }
+        return dummy.next
+    }
+}

7ab8ff63bd270176a68d830549b4431e63ac07ac
Iusupov Ramazan <iusupov.ramazan.i@gmail.com>
2026-08-31 11:23:59 -0700

    Add: subtree-of-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-4.swift
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func isSubtree(_ root: TreeNode?, _ subRoot: TreeNode?) -> Bool {
+        if subRoot == nil { return true }
+        guard let root else { return false }
+        let isLeftSubtree = isSubtree(root.left, subRoot)
+        let isRightSubtree = isSubtree(root.right, subRoot)
+        return isSameTree(root, subRoot) || isLeftSubtree || isRightSubtree
+    }
+    
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil { return true }
+        guard let p, let q, p.val == q.val else { return false }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+}

61e0314cdfe9e5af7d70a94fb6b5f94f4b92440a
Iusupov Ramazan <iusupov.ramazan.i@gmail.com>
2026-09-09 14:07:06 -0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var dict: [Int: Int] = [:]
+        for i in nums { dict[i, default: 0] += 1 }
+        return dict
+                .sorted(by: { $0.value > $1.value })
+                .prefix(k)
+                .map { $0.key }
+    }
+}

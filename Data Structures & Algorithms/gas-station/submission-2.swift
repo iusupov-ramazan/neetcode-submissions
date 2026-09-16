f60c43cdaf9e534aca524b22ca6aff10d78ee9a4
Iusupov Ramazan <iusupov.ramazan.i@gmail.com>
2026-09-16 11:49:54 -0700

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
+        if gas.reduce(0, +) < cost.reduce(0, +) { return -1 }
+        var result = 0
+        var total = 0
+        for i in 0..<gas.count {
+            total += gas[i] - cost[i]
+            if total < 0 { 
+                total = 0
+                result = i + 1
+            }
+        }
+        return result
+    }
+}
